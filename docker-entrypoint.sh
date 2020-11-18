8d63f56fabdd93a155614f1c07c57544818ae217
Daniel Rose <daniel-rose@gmx.de>
2020-11-18 08:58:17 +0100

    allow to call dandelion bin directly by docker run

---
+++ docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+isCommand() {
+  if [ "$1" = "sh" ]; then
+    return 1
+  fi
+
+  dandelion help "$1" > /dev/null 2>&1
+}
+
+if [ "${1#-}" != "$1" ]; then
+  set -- dandelion "$@"
+eif [ "$1" = 'dandelion' ]; then
+  set -- "$@"
+elif isCommand "$1"; then
+  set -- dandelion "$@"
+fi
+
+exec "$@"
